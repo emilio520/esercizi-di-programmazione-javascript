63e4d862f40434d03e0541a471435c3b378a0070
emilio520 <tuti.edgar@gmail.com>
2021-01-23 11:41:20 +0100

    master

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,48 @@
 
   http://www.imparareaprogrammare.it
 */
+var Voto=30
+switch (Confronto(Voto)) {
+  case 1:
+    console.log('Insufficiente');
+    break;
+  case 2:
+     console.log('Sufficiente');
+     break;
+  case 3:
+    console.log('Buono');
+    break;
+  case 4:
+    console.log('Distinto');
+    break;
+  case 5:
+    console.log('Ottimo');
+    break;
+  case 6:
+    console.log('Eccelente');
+    break;
+  default:
+    console.log('Valore inserito non valido');
+    break;
+}
+
+function Confronto(Voto) { 
+  if (Voto<18) {
+    return 1
+  }
+  if (Voto>=18 && Voto<21) {
+    return 2
+  }
+  if (Voto>=21 && Voto<24) {
+    return 3
+  }
+  if (Voto>=24 && Voto<27) {
+    return 4
+  }
+  if (Voto>=27 && Voto<29) {
+    return 5
+  }
+  if (Voto==30) {
+    return 6
+  }
+}
